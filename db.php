4e99c1788b6f98b7a60a4898decc8a3fe39634e8
Roger Fracé <rogerfrace@gmail.com>
2018-01-24 20:56:55 -0600

    readonly user password edit

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?
 $db_name = "fraceart2";
 $table_name = "prints";
-$connection = mysql_connect("fraceart2.db.8381461.hostedresource.com","artfrace2","Beng4lTig3r") or die("Could not connect to db");
+$connection = mysql_connect("fraceart2.db.8381461.hostedresource.com","artfrace2","Beng4lT!g3r") or die("Could not connect to db");
 $db = mysql_select_db($db_name, $connection) or die("Could not select database");
 ?>
--- db2.php5
+++ db2.php5
@@ -2,5 +2,5 @@
 //old creds
 //$db = new mysqli('mysql5.secureserver.net', 'fraceart' ,'fuzzg0th', 'fraceart');
 //new creds
-$db = new mysqli('fraceart2.db.8381461.hostedresource.com', 'artfrace2' ,'Beng4lTig3r', 'fraceart2');
+$db = new mysqli('fraceart2.db.8381461.hostedresource.com', 'artfrace2' ,'Beng4lT!g3r', 'fraceart2');
 ?>
